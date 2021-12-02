4201745ba7cbd6438c3d03c2851eac4d4d5fc8fc
nebojsagvozden <Nebojsa.Gvozden@dmv.ca.gov>
2021-12-02 08:09:57 -0800

    commited changes to MyProjectClass

--- MyProjectClass.java
+++ MyProjectClass.java
@@ -1,6 +1,7 @@
 public class MyProjectClass{
 	public static void main(String[] args){
 		System.out.println("Class created for GitBash tutorial");
+		System.out.println("Change has been made on MyProjectClass");
        }
 } 
 
