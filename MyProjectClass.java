d18834497d9ebfc2b4e7f5bd28e6c097b84dfec2
nebojsagvozden <mwnxg5@ISD-SCM-L03.ad.dmv.ca.gov>
2021-12-06 08:22:05 -0800

    Monday Commit

--- MyProjectClass.java
+++ MyProjectClass.java
@@ -2,6 +2,8 @@ public class MyProjectClass{
 	public static void main(String[] args){
 		System.out.println("Class created for GitBash tutorial");
 		System.out.println("Change has been made on MyProjectClass");
+		System.out.println("Another Change has been made on MyProjectClass");
+		System.out.println("More Changes");
        }
 } 
 
