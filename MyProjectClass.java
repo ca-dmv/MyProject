d19fb15ea1507dc5c738df69f4050f5753953470
nebojsagvozden <mwnxg5@ISD-SCM-L03.ad.dmv.ca.gov>
2021-12-06 16:38:14 -0800

    Committed println changes.

--- MyProjectClass.java
+++ MyProjectClass.java
@@ -2,6 +2,7 @@ public class MyProjectClass{
 	public static void main(String[] args){
 		System.out.println("Class created for GitBash tutorial");
 		System.out.println("Change has been made on MyProjectClass");
+		System.out.println("Another Change in Eclipse");
        }
 } 
 
